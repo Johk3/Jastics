f8d1e67380d50df19cefb3aa0aac8b214feea99c
Johannes Leppäkorpi <user3610@protonmail.com>
2020-05-31 01:31:51 +0300

    Init
    Signed-off-by: Johannes Leppäkorpi <user3610@protonmail.com>

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"fmt"
+)
+
+
+func main() {
+	input := "/home/johk/go/src/Jastics/feedback.json"
+	output := "/tmp/Jastics/"
+	fmt.Println("Starting...")
+
+	run(input, output)
+}

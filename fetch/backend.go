9cd74c2ae886ef48e18997fcbab2732dd0610453
Johannes Leppäkorpi <user3610@protonmail.com>
2020-06-01 01:20:49 +0300

    Package management + able to fetch json files
    Signed-off-by: Johannes Leppäkorpi <user3610@protonmail.com>

---
+++ fetch/backend.go
@@ -0,0 +1,41 @@
+package fetch
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"log"
+	"reflect"
+)
+
+
+func Run(input string, output string) map[int]int {
+	results := make(map[int]int)
+
+	fmt.Printf("Statistically analyzing %v, and dumping to %v\n", input, output)
+	jsonFile, err := os.Open(input)
+	check(err)
+
+	// defer close the json file so it can be parsed later
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var result interface{}
+	err = json.Unmarshal([]byte(byteValue), &result)
+	check(err)
+
+	_result := reflect.ValueOf(result)
+	for i := 0; i < _result.Len(); i++ {
+		fmt.Println(_result.Index(i))
+		break
+	}
+
+	return results
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatalln("Jastics error: ", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
-package jastics 
+package main
 
 import(
 	"fmt"
+	"Jastics/fetch"
 )
 
 
 func main() {
+	// This program is mainly used to analyze, and create graphs out of
+	// The user data of TextBreakerAlpha
+	// Most of the work is done with Go, though sentiment analysis is done with python
+
 	input := "/home/johk/go/src/Jastics/feedback.json"
 	output := "/tmp/Jastics/"
 	fmt.Println("Starting...")
 
-	run(input, output)
+	results := fetch.Run(input, output)
+	fmt.Println(results)
 }

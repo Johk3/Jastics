c7c5e34d538bef0a073b6262247eff76881b04da
Johannes Leppäkorpi <user3610@protonmail.com>
2020-06-01 16:13:23 +0300

    Able to now get datapoints
    Signed-off-by: Johannes Leppäkorpi <user3610@protonmail.com>

--- fetch/backend.go
+++ fetch/backend.go
@@ -10,8 +10,24 @@ import (
 )
 
 
-func Run(input string, output string) map[int]int {
-	results := make(map[int]int)
+type Feedback struct {
+	ratios []int
+	text []string
+}
+
+func (f *Feedback) AppendRatios(ratios []int) {
+	f.ratios = append(f.ratios, ratios...)
+}
+
+func (f *Feedback) AppendText(text string) {
+	f.text = append(f.text, text)
+}
+
+// -- -- --
+
+
+func Run(input string, output string) *Feedback {
+	modelF := Feedback{}
 
 	fmt.Printf("Statistically analyzing %v, and dumping to %v\n", input, output)
 	jsonFile, err := os.Open(input)
@@ -27,14 +43,25 @@ func Run(input string, output string) map[int]int {
 
 	_result := reflect.ValueOf(result)
 	for i := 0; i < _result.Len(); i++ {
-		for _, e := range _result.MapKeys() {
-			v := _result.MapIndex(e)
-			fmt.Println(v)
+		for _, e := range _result.Index(i).Elem().MapKeys() {
+			// Handle operations to fetch ratio and feedbacktext from feedback
+			if e.String() == "feedback" {
+				v := _result.Index(i).Elem().MapIndex(e)
+				for _, f := range v.Elem().MapKeys() {
+					if f.String() == "mainFormText" {
+						modelF.AppendText(v.Elem().MapIndex(f).String())
+					}
+					if f.String() == "ratios" {
+						fmt.Println(v.Elem().MapIndex(f))
+					}
+				}
+			}
 		}
+		fmt.Printf("%d/%d\n", i, _result.Len())
 		break
 	}
 
-	return results
+	return &modelF
 }
 
 func check(err error) {

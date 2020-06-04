7febff100eee0136661dac791eb8ef94306c57b1
Johannes Leppäkorpi <user3610@protonmail.com>
2020-06-05 00:40:48 +0300

    JANLP python sentiment analysis module working and able to process feedback
    Signed-off-by: Johannes Leppäkorpi <user3610@protonmail.com>

--- src/sentiment.py
+++ src/sentiment.py
@@ -2,15 +2,30 @@ from textblob import TextBlob
 
 
 class JANLP:
-    def __init__(self, feedbackFile):
-        self.feedbackFile = feedbackFile
-        self.lines = ""
+    """JANLP, provides statistical analysis for jastonmatter.com"""
+    def __init__(self, feedback_file):
+        self.feedback_file = feedback_file
+        self.lines = []
 
-        with open(self.feedbackFile, "r") as f: self.lines = f.readlines()
-    def getSentiment(self):
-        print(self.lines)
+        with open(self.feedback_file, "r") as f:
+            self.lines = f.read().split("-+--++--+-+--++--+--+-+--+")
+
+    def get_sentiment(self):
+        """Produce the sentiment values for each feedback given"""
+        polarityAvg = 0
+        subjectivityAvg = 0
+
+        for line in self.lines:
+            context = TextBlob(line)
+            polarityAvg += context.sentiment.polarity
+            subjectivityAvg += context.sentiment.subjectivity
+
+        polarityAvg = polarityAvg/len(self.lines)*1000
+        subjectivityAvg = subjectivityAvg/len(self.lines)*1000
+
+        print(f"{round(polarityAvg, 2)}% Positive, and {round(subjectivityAvg, 2)}% Subjective")
 
 
 if __name__ == "__main__":
     Jastics = JANLP("/tmp/jm_feedback")
-    Jastics.getSentiment()
+    Jastics.get_sentiment()
